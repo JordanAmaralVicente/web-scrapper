8d369a33e751452ada1eadb7dd22449064b4f970
JordanAmaralVicente <jordanitalovicente@gmail.com>
2022-01-03 21:43:56 -0300

    refactor: using a headless scrapper in main_scrapper

--- webscrapper/main_scrapper.py
+++ webscrapper/main_scrapper.py
@@ -1,42 +1,27 @@
-#!/usr/bin/env python3
-# -*- coding: utf -*- 
-import os
-import time
+import asyncio
 import pandas as pd
+from pyppeteer import launch
 from bs4 import BeautifulSoup
-from selenium import webdriver
 from utils import save_result_as_excel
-from filter_data import main_filter
 
-def os_exclude_directory():
-    os.system("rm -rf ../excel_tables")
-    os.system("mkdir ../excel_tables")
 
-def main_scrapper():
-    fundamentus_url = 'https://fundamentus.com.br/resultado.php'
-    
-    driver = webdriver.Chrome('../chromeDriver/chromedriver')
+async def getDataFromWebPage(url):
+    browser = await launch(headless=True)
+    page = await browser.newPage()
 
-    driver.get(fundamentus_url)
-    time.sleep(2)
+    response = await page.goto(url, {'waitUntil': 'networkidle2'})
+    html = await response.text()
+    await browser.close()
 
-    html = driver.page_source
-
-    driver.close()
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find('table', {'id': 'resultado'})
 
     df = pd.read_html(str(table))[0]
-
-    os_exclude_directory()
-    save_result_as_excel(df, "../excel_tables/output", False)
-    
-    proceed = input('Deseja prosseguir para o próximo passo ? [Y/n]: ')
-    if proceed == 'Y':
-        main_filter()
-    else:
-        return 
+    return df
 
 
 if __name__ == '__main__':
-    main_scrapper()
+    fundamentus_url = 'https://fundamentus.com.br/resultado.php'
+
+    df = asyncio.get_event_loop().run_until_complete(getDataFromWebPage(fundamentus_url))
+    save_result_as_excel(df, "../excel_tables/output", with_date=False)
